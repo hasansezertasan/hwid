eb2d7d064ec21ee74dc546feae9dd3a386c67ba8
hasansezertasan <hasansezertasan@gmail.com>
2025-08-03 04:24:23 +0300

    feat: implement HWID extraction functionality with platform-specific support for Linux, Darwin, and Win32; add logging and exception handling

---
+++ src/hwid/logger.py
@@ -0,0 +1,9 @@
+"""Logging module for HWID."""
+
+# Copyright (c) 2023 Hasan Sezer Taşan
+# Licensed under the MIT License
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
